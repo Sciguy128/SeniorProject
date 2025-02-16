dfbcf1f0ea3c6cc5cbf481d109ce5a9b82917475
abl57 <abl77@case.edu>
2025-02-15 20:39:04 -0500

    Simple database integration test

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,23 @@
 import time
-from flask import Flask#
+from flask import Flask, jsonify
 from flask_cors import CORS
+import psycopg2
 
+# This will have to change for outside my local machine
+database_url = "postgres://postgres:crowdsearch@localhost:5433/crowd_search"
 app = Flask(__name__)
 
 CORS(app)
 
+conn = psycopg2.connect(database_url)
+cur = conn.cursor()
+
 @app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/api/users')
+def get_users():
+    cur.execute("SELECT * FROM USERS")
+    users = cur.fetchall()
+    return jsonify(users)
