41b20b522ba18951afa364c8b67b31e5fe2d4736
RyanL72 <105567147+RyanL72@users.noreply.github.com>
2025-03-28 16:44:35 -0400

    update ios

---
+++ ios/CrowdSearch/CrowdSearch/CrowdSearchApp.swift
@@ -0,0 +1,29 @@
+//
+//  CrowdSearchApp.swift
+//  CrowdSearch
+//
+//  Created by Ryan Lin on 3/28/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct CrowdSearchApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
